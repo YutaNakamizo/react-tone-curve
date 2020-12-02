4332c4de666774a9fdde2c642d058cf1e7dc33fc
Yuta Nakamizo <yuta.nakamizo@ggtk.app>
2020-12-02 16:05:24 +0900

    demo settings

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'demo/src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(cjs|mjs|js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|ico)$/,
+        use: 'url-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'demo/src/index.html'),
+      filename: './index.html',
+    }),
+  ],
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+    ],
+  },
+  devServer: {
+    port: 3000,
+  },
+};
+
