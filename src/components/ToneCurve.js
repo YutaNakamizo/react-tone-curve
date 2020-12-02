ba09c016419c777ef2e4c13706f05ec717bf911e
Yuta Nakamizo <yuta.nakamizo@ggtk.app>
2020-12-02 15:38:39 +0900

    lib transpile base

---
+++ src/components/ToneCurve.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ToneCurveArea } from './ToneCurve/Area';
+
+export const ToneCurve = () => {
+  return (
+    <ToneCurveArea
+    />
+  );
+};
+
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { ToneCurve } from './components/ToneCurve';
+
+export default ToneCurve;
+
